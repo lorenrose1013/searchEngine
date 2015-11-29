19e88de4e4e5c5049909c885a896fda13c704d10
lorenrose1013 <lorenrose1013@aol.com>
2015-11-28 20:27:55 -0500

    fixed who and added stop words

--- utils.py
+++ utils.py
@@ -4,66 +4,25 @@ def getTenPages(query):
 	results = google.search(query, num=10, start = 0, stop = 10)
 	pages = []
 	for url in results:
-            page = urllib2.urlopen(url)
-            page = page.read()
-            pages.append(page)
-        return pages
-
-#def getURLSoup(url):
-#	page = urllib2.urlopen(url)
-#	page = page.read()
-#	soup = bs4.BeautifulSoup(page, 'html')
-#	return soup
-
-#def getTenPages(query):
-#        urls = getTenURLS(query)
-#        soups = []
-#        for url in urls:
-#                soups.append(getURLSoup(url)
-#        return soup
-	
-
-def getURLSoup(url):
-	page = urllib2.urlopen(url)
-	page = page.read()
-	soup = bs4.BeautifulSoup(page, 'html')
-	return soup
-
-def getTenSoup(query):
-        urls = getTenURLS(query)
-        soups = []
-        for url in urls:
-            soups.append(getURLSoup(url)
-        return soups
-
-
-
-if __name__ == "__main__": 
-	#urls =  getTenURLS("who played spiderman")
-	#print getURLSoup(urls[0]).prettify()
-    print who("who played spiderman")
-
-
-if __name__ == "__main__": 
-	urls =  getTenURLS("who played spiderman")
-	print getURLSoup(urls[0]).prettify()
-    
+		url = urllib2.Request(url, headers={'User-Agent' : "Bot? What Bot?"})
+		responce = urllib2.urlopen(url)
+		page = responce.read()
+		pages.append(page)
+	return pages
 
 def who(query):
-        pages = getTenPages(query)
-        finds ={}
-        nameSub1 = "[A-Z][a-z]? [A-Z][a-z]?"
-        for page in pages:        
-                results = re.sub("", "", page)
-                for r in results:
-                        if r in finds:
-                            finds[r] += 1
-                        else:
-                            finds[r] = 1
-        ##bucket style
-        print finds
-        #parse for who
-        
+	pages = getTenPages(query)
+	finds ={}
+	nameSub1 = "[A-Z][a-z]+ [A-Z][a-z]+"
+	for page in pages:        
+		results = re.findall(nameSub1, page)
+		for r in results:
+			if r in finds:
+				finds[r] += 1
+			else:
+					finds[r] = 1
+	print max(finds, key=finds.get)
+
 def when(query):
     pages = getTenPages(query)
     finds = {}
@@ -72,14 +31,14 @@ def when(query):
     ##    results = re.sub("","",page)
 
 def where(query):
-    soups = getTenSoup(query)
+    pages = getTenPages(query)
 
 def getResult(query):
-    if "when" in query:
-        return when(query)
-	elif "who" in query:
+	if "when" in query.lower():
+		return when(query)
+	elif "who" in query.lower():
 		return who(query)
-    elif "where" in query:
+	elif "where" in query.lower():
 		return where(query)
 	##elif "what" in query:
 	##	return what(query)
@@ -87,3 +46,11 @@ def getResult(query):
     ##    return why(query)
 	else:
 		return "Sorry, can you rephrase your query as a Who, Where, or When question?"
+
+
+
+
+if __name__ == "__main__": 
+	#urls =  getTenURLS("who played spiderman")
+	#print getURLSoup(urls[0]).prettify()
+    print who("Who did 9/11?")
